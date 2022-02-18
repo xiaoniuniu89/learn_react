0b50192ad94b7d4644b70b000e91e91049828751
Daniel Callaghan <dan_cal89@hotmail.com>
2022-02-18 17:41:13 +0000

    change to new repo

---
+++ React-Firebase-lesson-69/multipage-site/src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa debitis sequi numquam ea hic deserunt expedita voluptates. Quibusdam debitis expedita nesciunt aliquid ex maiores odit aut quis ipsa. Cum, assumenda!</p>
+    </div>
+  )
+}
---
+++ React-Firebase-lesson-69/multipage-site/src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+        <h2>Contact</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa debitis sequi numquam ea hic deserunt expedita voluptates. Quibusdam debitis expedita nesciunt aliquid ex maiores odit aut quis ipsa. Cum, assumenda!</p>
+        
+    </div>
+  )
+}
